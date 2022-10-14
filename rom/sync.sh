79779c98f41c41ba6684eb1f5639caa907fec117
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-14 07:53:38 +0100

    Update sync.sh

--- rom/sync.sh
+++ rom/sync.sh
@@ -2,7 +2,7 @@
 
 # install package..
 sudo apt update -y
-sudo apt install bc sudo git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python-is-python3 -y
+sudo apt install bc libreadline7 sudo git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python-is-python3 -y
 # run repo..
 
 sudo mkdir ~/bin
